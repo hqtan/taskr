538f273e40bd6babdc947d2034c4cc4794e8b918
hqtan <hanqing.tan@gmail.com>
2014-04-09 12:28:38 +0000

    add spinner package for loading template; add description attribute for each task in fixtures; add links task page for each task

--- server/fixtures.js
+++ server/fixtures.js
@@ -4,19 +4,22 @@ if (Tasks.find().count() === 0) {
   Tasks.insert({
     title: 'First Task',
     requester: 'qing',
-    submitted: now - 7 * 3600 * 1000
+    submitted: now - 7 * 3600 * 1000,
+    description: 'Do something for first task'
   });
 
   Tasks.insert({
     title: 'Second Task',
     requester: 'jeromy',
-    submitted: now - 6 * 3600 * 1000
+    submitted: now - 6 * 3600 * 1000,
+    description: 'Do something for second task'
   });
 
   Tasks.insert({
     title: 'Third Task',
     requester: 'qing',
-    submitted: now - 5 * 3600 * 1000
+    submitted: now - 5 * 3600 * 1000,
+    description: 'Do something for third task'
   });
 
 }
