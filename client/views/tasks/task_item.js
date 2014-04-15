d4a3c835f1ed526c319415f32a14bf939a6c6357
hqtan <hanqing.tan@gmail.com>
2014-04-15 11:35:31 +0000

    update template and template manager with task edit feature

--- client/views/tasks/task_item.js
+++ client/views/tasks/task_item.js
@@ -1,5 +1,10 @@
 Template.taskItem.helpers({
   submittedText: function() {
     return new Date(this.submitted).toString();
+  },
+
+  ownTask: function() {
+    return this.userId == Meteor.userId();
   }
+
 });
